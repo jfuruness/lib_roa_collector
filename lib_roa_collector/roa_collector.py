36a302084d55d76e4a8e56cb5f7dd08bd36ce64e
Justin Furuness <jfuruness@gmail.com>
2021-08-22 23:30:58 +0000

    pushing changes

--- lib_roa_collector/roa_collector.py
+++ lib_roa_collector/roa_collector.py
@@ -4,47 +4,39 @@ from multiprocessing import cpu_count
 import os
 import re
 
+from lib_utils.base_classes import Base
 from lib_utils.file_funcs import makedirs, download_file
-from lib_utils.helper_funcs import get_hrefs, run_cmds, mp_call
+from lib_utils.helper_funcs import get_hrefs, run_cmds
 
 from .tables import ROAsTable
 
-class ROACollector:
+class ROACollector(Base):
     """Downloads ROAs from ripe"""
 
     url = "https://ftp.ripe.net/rpki/"
 
-    def __init__(self, base_roa_path="/ssd/roas"):
-        """Initializes ROA path"""
-
-        self.base_roa_path = base_roa_path
-        makedirs(base_roa_path, remake=True)
-        self.tsv_path = os.path.join(base_roa_path, "roas.tsv")
-
-    def run(self, dl_time=datetime.utcnow() - timedelta(days=2),
-            dl_cpus=cpu_count() * 4,
-            database=True):
+    def run(self):
         """Gets URLs, downloads files, and concatenates them"""
 
         # Get URLs and paths
-        urls = self._get_urls(dl_time)
+        urls = self._get_urls()
         paths = self._get_paths(urls)
         # Download all files with multiprocessing
-        mp_call(download_file, [urls, paths], "Downloading", cpus=dl_cpus)
+        self.download_mp(download_file, [urls, paths])
         # Extract data to the tsv_path
         self._extract_data()
         # Insert info into the db
-        if database:
+        if self.db:
             with ROAsTable(clear=True) as db:
                 db.bulk_insert_tsv(self.tsv_path)
 
-    def _get_urls(self, dl_time):
+    def _get_urls(self):
         """Gets URLs to all the ROA CSVs"""
 
         # Get URLs of Tals
-        tal_urls = [self.url + x for x in get_hrefs(self.url) if ".tal" in x]
+        urls = [self.url + x for x in get_hrefs(self.url) if ".tal" in x]
         # https://ftp.ripe.net/rpki/afrinic.tal/2021/08/16/roas.csv
-        return [dl_time.strftime(f"{x}/%Y/%m/%d/roas.csv") for x in tal_urls]
+        return [self.dl_time.strftime(f"{x}/%Y/%m/%d/roas.csv") for x in urls]
  
     def _get_paths(self, urls):
         """Gets all paths from the URLs for downloading"""
@@ -54,7 +46,7 @@ class ROACollector:
             # Get name of tal
             tal = re.findall("rpki/(.+).tal/", url)[0]
             # Get download path
-            paths.append(os.path.join(self.base_roa_path, f"{tal}.csv"))
+            paths.append(os.path.join(self._dir, f"{tal}.csv"))
         return paths
 
     def _extract_data(self):
@@ -62,8 +54,9 @@ class ROACollector:
 
         rows = []
         # Done this way because many links are broken, so paths are empty
-        for fname in os.listdir(self.base_roa_path):
-            path = os.path.join(self.base_roa_path, fname)
+        for fname in os.listdir(self._dir):
+            tal = fname.replace(".csv", "")
+            path = os.path.join(self._dir, fname)
             with open(path, "r") as f:
                 for row in csv.DictReader(f):
                     # Get rid of quotes
@@ -74,7 +67,8 @@ class ROACollector:
                                "prefix": row["IP Prefix"],
                                "max_length": row["Max Length"],
                                "not_before": row["Not Before"],
-                               "not_after": row["Not After"]}
+                               "not_after": row["Not After"],
+                               "tal": tal}
                     rows.append(new_row)
         # Write to file
         with open(self.tsv_path, "w") as f:
